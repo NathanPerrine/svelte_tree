d4f5e02c82b10ef3104f108a795f5313a4fe3040
Nathan <nathan.perrine@gmail.com>
2023-08-10 01:43:14 -0400

    edit profile page + unplugin icons

--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
 * overflow during transitions during login
     * Card changes to incoming card immediately when switching
         * Should switch with a skeleton layout
+
+* check / x icon before adding a link in profile
+* tooltip icon for url validation rules
---
+++ src/routes/[username]/edit/+page.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import UserLink from "$lib/components/UserLink.svelte";
+    import { db, userData, user } from "$lib/firebase";
+    import { arrayRemove, arrayUnion, doc, setDoc, updateDoc } from "firebase/firestore";
+    import { writable } from "svelte/store";
+    import MingcuteInformationLine from '~icons/mingcute/information-line'
+
+    const icons = ["Twitter", "YouTube", "TikTok", "LinkedIn", "GitHub", "Custom"];
+
+    const formDefaults = {
+        icon: "custom",
+        title: "",
+        url: "https://"
+    };
+
+    const formData = writable(formDefaults);
+
+    let showForm = false;
+
+    $: urlIsValid = $formData.url.match(/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/);
+    $: titleIsValid = $formData.title.length < 20 && $formData.title.length > 0;
+    $: formIsValid = urlIsValid && titleIsValid;
+
+    async function addLink(e: SubmitEvent) {
+        const userRef = doc(db, "users", $user!.uid)
+
+        await updateDoc(userRef, {
+            links: arrayUnion({
+                ...$formData,
+                id: Date.now().toString(),
+            }),
+        });
+
+        formData.set({
+            icon: "",
+            title: "",
+            url: "",
+        });
+
+        showForm = false;
+    }
+
+    async function deleteLink(item: any) {
+        const userRef = doc(db, "users", $user!.uid);
+        await updateDoc(userRef, {
+            links: arrayRemove(item),
+        });
+    }
+
+    function cancelLink() {
+        formData.set(formDefaults);
+        showForm = false;
+    }
+
+</script>
+
+<main class="max-w-xl mx-auto">
+    {#if $userData?.username == $page.params.username}
+        <h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">
+        Edit your Profile
+        </h1>
+
+        <!-- INSERT sortable list here -->
+
+        {#if showForm}
+        <form
+            on:submit|preventDefault={addLink}
+            class="bg-base-200 p-6 w-full mx-auto rounded-xl"
+        >
+            <select
+            name="icon"
+            class="select select-sm"
+            bind:value={$formData.icon}
+            >
+
+            {#each icons as icon}
+                <option value={icon.toLowerCase()}>{icon}</option>
+            {/each}
+            </select>
+            <input
+            name="title"
+            type="text"
+            placeholder="Title"
+            class="input input-sm"
+            bind:value={$formData.title}
+            />
+            <input
+            name="url"
+            type="text"
+            placeholder="URL"
+            class="input input-sm"
+            bind:value={$formData.url}
+            />
+            <div class="my-4">
+                <MingcuteInformationLine class='text-info'/>
+                {#if formIsValid}
+                    <p class="text-success text-xs">Looks good!</p>
+                {:else}
+                    <p class="text-info text-xs" class:text-success={titleIsValid}> Titles must be between 1 and 20 characters.</p>
+                    <p class="text-info text-xs" class:text-success={urlIsValid}>URL must start with http(s) and follow the format https://www.google.com</p>
+                {/if}
+            </div>
+
+            <button
+            disabled={!formIsValid}
+            type="submit"
+            class="btn btn-success block">Add Link</button
+            >
+
+            <button type="button" class="btn btn-xs my-4" on:click={cancelLink}>Cancel</button>
+        </form>
+        {:else}
+        <button
+            on:click={() => (showForm = true)}
+            class="btn btn-outline btn-info block mx-auto my-4"
+        >
+            Add a Link
+        </button>
+        {/if}
+    {/if}
+</main>
